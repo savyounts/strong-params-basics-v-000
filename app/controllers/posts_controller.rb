43a8da03e2711c4b141f3cb8925128d836726b11
Savannah Younts <savyounts@gmail.com>
2018-09-20 15:50:56 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 	end
 
 	private 	
-		def post_params
-			params.require(:post).permit(:title, :description)
+		def post_params(*args)
+			params.require(:post).permit(*args)
 		end
 end
